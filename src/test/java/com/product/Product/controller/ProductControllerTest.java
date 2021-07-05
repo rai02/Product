8814cd737cf07f011043ef598438f846c81ffe2f
Deva <barrypythondev@gmail.com>
2021-07-05 18:43:59 +0530

    added custom exception handler for validation

---
+++ src/test/java/com/product/Product/controller/ProductControllerTest.java
@@ -0,0 +1,31 @@
+package com.product.Product.controller;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void fetchProductList() throws Exception {
+        mockMvc.perform(get("/products")).andExpect(status().isOk());
+    }
+
+    @Test
+    void saveProductSuccessful() throws Exception {
+        mockMvc.perform(post("/products")).andExpect(status().isOk());
+    }
+}
